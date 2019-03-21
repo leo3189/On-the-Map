30c88f3ec896da65f7a5502ae615f350f228c9f2
leonard <>
2019-03-21 05:19:18 -0700

    no message

---
+++ On The Map/controller/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  On The Map
+//
+//  Created by leonard on 3/21/19.
+//  Copyright © 2019 leonard. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+
+}
---
+++ On The Map/view/logInButtonView.swift
@@ -0,0 +1,17 @@
+//
+//  logInButtonView.swift
+//  On The Map
+//
+//  Created by leonard on 3/21/19.
+//  Copyright © 2019 leonard. All rights reserved.
+//
+
+import UIKit
+
+class logInButtonView: UIButton {
+
+    override func awakeFromNib() {
+        self.layer.cornerRadius = 5
+    }
+
+}
