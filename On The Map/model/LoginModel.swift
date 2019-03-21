8d49b0f6fcdbd2ad331e2060c31f1d492fc41611
leonard <>
2019-03-21 05:30:37 -0700

    no message

---
+++ On The Map/model/LoginModel.swift
@@ -0,0 +1,14 @@
+//
+//  LoginModel.swift
+//  On The Map
+//
+//  Created by leonard on 3/21/19.
+//  Copyright © 2019 leonard. All rights reserved.
+//
+
+import Foundation
+
+struct Login {
+    let email: String!
+    let password: String!
+}
---
+++ On The Map/view/logInTextFieldView.swift
@@ -0,0 +1,19 @@
+//
+//  logInTextFieldView.swift
+//  On The Map
+//
+//  Created by leonard on 3/21/19.
+//  Copyright © 2019 leonard. All rights reserved.
+//
+
+import UIKit
+
+class logInTextFieldView: UITextField {
+
+    override func awakeFromNib() {
+        self.layer.borderWidth = 1
+        self.layer.borderColor = UIColor.lightGray.cgColor
+        self.borderStyle = .none
+        self.layer.cornerRadius = 5
+    }
+}
